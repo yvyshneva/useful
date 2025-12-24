efcabecb25d42ac23378f1c162176f6c96b91f81
Yana Vyshneva <yana@apeironinsight.com>
2025-12-24 08:35:30 +0100

    Refactor time conversion logic to include weeks and days in output

--- Python/convert_float_days_to_full_days_and_hours.py
+++ Python/convert_float_days_to_full_days_and_hours.py
@@ -1,10 +1,15 @@
 hours = float(input("Enter the number of hours: "))
 
 working_days = int(hours // 8)
+
+working_weeks = int(working_days // 5)
+remaining_days = working_days % 5
+
 remaining_hours = round(hours % 8)
 
 units = (
-    f"{working_days}d" if working_days else None,
+    f"{working_weeks}w" if working_weeks else None,
+    f"{remaining_days}d" if remaining_days else None,
     f"{remaining_hours}h" if remaining_hours else None,
 )
 
